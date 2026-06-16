9f20a076681c70566085336d970395f3bad94806
Yuan <justyuan.z@gmail.com>
2026-06-16 12:59:46 +0300

    missing tests.

---
+++ cpp08/ex00/Easyfind.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+/*
+there is no universal type called:iterator
+different containers have different iterator types:
+std::vector<int>::iterator
+std::list<int>::iterator
+std::deque<int>::iterator
+because T is a template type, we must write: 
+typename T::iterator ("Compiler, T::iterator is a type.")
+*/
+template <typename T>
+typename T::iterator easyfind(T& container, int num)
+{
+	return std::find(container.begin(), container.end(), num);	
+}
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,42 @@
+#include"Easyfind.hpp"
+#include<iostream>
+#include<vector>
+#include<list>
+
+template <typename T>
+void test_easyfind(T& container, int num)
+{
+	typename T::iterator it = easyfind(container, num);
+	if (it != container.end())
+		std::cout << "Found:" << *it << std::endl;
+	else
+		std::cout << "Not found." << std::endl; 
+}
+
+int main(void)
+{
+	{
+		std::cout << "--------------test vector-------------" << std::endl;
+		std::vector<int> vec = {0, 1, 2, 3, 4};
+		test_easyfind(vec, 0);
+		test_easyfind(vec, 99);
+	}
+	{
+		std::cout << "--------------test list-------------" << std::endl;
+		std::list<int> list = {0, 1, 2, 3, 4};
+		test_easyfind(list, 0);
+		test_easyfind(list, 99);
+	}
+	{
+		std::cout << "--------------test list-------------" << std::endl;
+		std::list<int> list = {0, 1, 2, 3, 4};
+		test_easyfind(list, 0);
+		test_easyfind(list, 99);
+	}
+	{
+		std::cout << "--------------test deque-------------" << std::endl;
+		std::deque<int> de = {0, 1, 2, 3, 4};
+		test_easyfind(de, 0);
+		test_easyfind(de, 99);
+	}
+}
