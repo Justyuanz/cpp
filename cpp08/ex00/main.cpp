b2c8514e450226f2cd05c47f5fbd8754258e8fdf
Yuan <justyuan.z@gmail.com>
2026-06-16 13:29:02 +0300

    done:ex00

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 #include<vector>
 #include<list>
+#include<deque>
+#include<forward_list>
 
 template <typename T>
 void test_easyfind(T& container, int num)
@@ -38,5 +40,11 @@ int main(void)
 		std::deque<int> de = {0, 1, 2, 3, 4};
 		test_easyfind(de, 0);
 		test_easyfind(de, 99);
+	}
+	{
+		std::cout << "--------------test forward list-------------" << std::endl;
+		std::forward_list<int> f_list = {0, 1, 2, 3, 4};
+		test_easyfind(f_list, 0);
+		test_easyfind(f_list, 99);
 	}
 }
